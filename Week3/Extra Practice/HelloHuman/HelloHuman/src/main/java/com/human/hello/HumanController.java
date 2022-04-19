01dd057baa81af96beed4dcfd6ee71f9374e8dbb
maguina-sj <jomaguina90@gmail.com>
2022-04-19 14:11:05 -0700

    human done

---
+++ Week3/Extra Practice/HelloHuman/HelloHuman/src/main/java/com/human/hello/HumanController.java
@@ -0,0 +1,21 @@
+package com.human.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HumanController {
+	@RequestMapping("/")
+	public String index(@RequestParam (value="name", required=false)String name) {
+		if (name == null) {
+			return "Hello Human";
+		}
+		else {
+			return "Hello " + name;
+		}
+		
+	}
+	
+
+}
