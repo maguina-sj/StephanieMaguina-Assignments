46f6930cadd0082bef396f6aee2645f2c9246104
maguina-sj <jomaguina90@gmail.com>
2022-04-19 14:35:37 -0700

    pathvariables done

---
+++ Week3/Daikichi Path Variables/DaikichiPathVariables/src/main/java/com/pathvariables/daikichi/DaikichiPathVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.pathvariables.daikichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiPathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPathVariablesApplication.class, args);
+	}
+
+}
---
+++ Week3/Daikichi Path Variables/DaikichiPathVariables/src/test/java/com/pathvariables/daikichi/DaikichiPathVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathvariables.daikichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPathVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
