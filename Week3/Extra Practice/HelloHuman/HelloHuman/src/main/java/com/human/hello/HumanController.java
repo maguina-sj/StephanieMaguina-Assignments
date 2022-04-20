1ad71af0bfa0348b7ba1d70a79d9faabb05c9102
maguina-sj <jomaguina90@gmail.com>
2022-04-19 18:00:23 -0700

    human updated

--- Week3/Extra Practice/HelloHuman/HelloHuman/src/main/java/com/human/hello/HumanController.java
+++ Week3/Extra Practice/HelloHuman/HelloHuman/src/main/java/com/human/hello/HumanController.java
@@ -7,15 +7,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HumanController {
 	@RequestMapping("/")
-	public String index(@RequestParam (value="name", required=false)String name) {
+	public String index(@RequestParam (value="name", required=false)String name, @RequestParam (value="lastName", required=false)String lastName, @RequestParam (value="num", required=false) Integer num) {
+		String output="";
+//		if(name != null && lastName != null && num > 0 && num != null) {
+//			for (int i = 1; i <= num; i++) {
+//				output += "Hello " + name;
+//			}
+//		}
+//		else if(name != null && lastName != null) {
+//			output= "Hello " + name + " " + lastName;
+//		}
+//		else if (name == null && lastName == null) {
+//			output= "Hello Human";
+//		}
+//		
+//		
+//		else if (name != null || lastName != null) {
+//			output= "Hello " + name ;
+//		}
+//		else if (lastName != null) {
+//			output= "Hello " + lastName;
+//		}
+//		
+//		else {
+//			
+//		}
+//		return output;
+//	}
+//	
 		if (name == null) {
-			return "Hello Human";
+			name = "Human";
 		}
-		else {
-			return "Hello " + name;
+		if (lastName == null) {
+			lastName = "";
 		}
-		
+		if (num== null) {
+			num = 1;
+		}
+		String result = "";
+		for (int i = 0; i < num; i++) {
+			result += String.format("Hello %s %s<br>", name, lastName);
+		}
+		return result;
 	}
-	
-
 }
